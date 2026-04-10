c5d5fe03bf9e496dba7ab01bfc41cd6a6d32bae1
Pelle Lundgren <pellburt@gmail.com>
2026-04-10 06:49:46 +0200

    feat: explore game loop by creating a moving red square

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import p8 "github.com/drpaneas/pigo8"
 
-type game struct{}
+type game struct{
+    x, y float64
+}
 
-func (g *game) Init()   {}
-func (g *game) Update() {}
+func (g *game) Init()   {
+    g.x = 60
+    g.y = 60
+}
+func (g *game) Update() {
+    if p8.Btn(p8.LEFT) { g.x-- }
+    if p8.Btn(p8.RIGHT) { g.x++ }
+    if p8.Btn(p8.UP) { g.y-- }
+    if p8.Btn(p8.DOWN) { g.y++ }
+}
 
 func (g *game) Draw() {
-    p8.Cls(1)
-    p8.Print("hello, world!", 40, 60)
+    p8.Cls(0)
+    p8.Rectfill(g.x, g.y, g.x+8, g.y+8, 8) // Red square
 }
 
 
