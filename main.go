ca1c520b016eae6b1a85b29c4420ae754eaa406e
Pelle Lundgren <pellburt@gmail.com>
2026-05-08 07:19:08 +0200

    feat(pong): initialize positions

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ type Paddle struct {
     color int
 }
 
-type Ball stuct {
+type Ball struct{
     x,  y, size float64
     dx, dy  float64
     color int
@@ -20,7 +20,31 @@ type Game struct{
     playerScore, computerScore int
 }
 
-func (g *Game) Init() {}
+func (g *Game) Init() {
+
+    // Initialize positions
+
+    // Player paddle on the left
+    g.player = Paddle{
+        x: 4, y: 54,
+        width: 4, height: 20,
+        speed: 4, color: 12,
+    }
+
+    // Computer paddle on the right
+    g.computer = Paddle{
+        x: 120, y: 54,
+        width: 4, height: 20,
+        speed: 1.5, color: 8,
+    }
+
+    // Ball in the center
+    g.ball = Ball{
+        x: 64, y: 32, size: 4,
+        dx: 2, dy: 1,
+        color: 7,
+    }
+}
 
 func (g *Game) Update() {}
 
