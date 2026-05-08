72146652e4dfa0b5471ef9aa4a1630263097e646
Pelle Lundgren <pellburt@gmail.com>
2026-05-08 07:10:37 +0200

    feat(pong): project setup

--- main.go
+++ main.go
@@ -2,25 +2,17 @@ package main
 
 import p8 "github.com/drpaneas/pigo8"
 
-type game struct{}
+type Game struct{}
 
-func (g *game) Init() {}
+func (g *Game) Init() {}
 
-func (g *game) Update() {}
+func (g *Game) Update() {}
 
-func (g *game) Draw() {
-    p8.Cls(1)  // Dark blue background
-    
-    // Draw sprite normally
-    p8.Spr(1, 40, 60)
-    
-    // Draw same sprite with yellow transparent (shows background)
-    p8.Palt(10, true)  // Yellow transparent
-    p8.Spr(1, 80, 60)
-    p8.Palt()  // Reset
+func (g *Game) Draw() {
+  p8.Cls(0)
 }
 
 func main() {
-    p8.InsertGame(&game{})
+    p8.InsertGame(&Game{})
     p8.Play()
 }
